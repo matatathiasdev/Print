5e040678b328b54b96c325d791a4473d458df077
matatathiasdev <cordeiro.data.science@gmail.com>
2024-02-15 10:47:37 -0300

    retgirada do a

--- PRINT.py
+++ PRINT.py
@@ -46,13 +46,13 @@ def Print():
     if util.find_spec("pyautogui") is not None:
         from pyautogui import press
         press("ctrl")
-        press("a")
+        # press("a")
         time.sleep(120)
     else:
         subprocess.check_call(["pip", "install", pacote])
         from pyautogui import press
         press("ctrl")
-        press("a")
+        # press("a")
         time.sleep(120)
 # VALOR INICIAL
 valor = LerTxt()
